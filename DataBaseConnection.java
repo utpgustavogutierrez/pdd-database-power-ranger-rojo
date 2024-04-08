3130f705b032520b7014488e5cdc41457a730486
DanielarenasL <124918183+DanielarenasL@users.noreply.github.com>
2024-04-08 04:36:07 +0000

    Implementacion Singleton

---
+++ DataBaseConnection.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    private Connection connection;
+    private String url = "jdbc:sqlite:sample.db";
+
+    private DatabaseConnection() throws SQLException {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            this.connection = DriverManager.getConnection(url);
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Database Connection Creation Failed : " + ex.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static DatabaseConnection getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new DatabaseConnection();
+        }
+
+        return instance;
+    }
+}
--- Sakila.java
+++ Sakila.java
@@ -1,33 +1,23 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Sakila
-{
-  public static void main(String[] args)
-  {
-    // NOTE: Connection and Statement are AutoClosable.
-    //       Don't forget to close them both in order to avoid leaks.
-    try
-    (
-      // create a database connection
-      Connection connection = DriverManager.getConnection("jdbc:sqlite:./sqlite-sakila.db");
+public class Sakila {
+  public static void main(String[] args) {
+    try {
+      Connection connection = DatabaseConnection.getInstance().getConnection();
       Statement statement = connection.createStatement();
-    )
-    {
+
       statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
       ResultSet rs = statement.executeQuery("select * from film");
-      while(rs.next())
-      {
+      while(rs.next()) {
         // read the result set
         System.out.println("title = " + rs.getString("title"));
       }
-    }
-    catch(SQLException e)
-    {
+    } 
+    catch(SQLException e) {
       // if the error message is "out of memory",
       // it probably means no database file is found
       e.printStackTrace(System.err);
